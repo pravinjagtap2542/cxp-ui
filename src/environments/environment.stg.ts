030df0828ae3eba042d5cf631dee7185bca69237
pjagtap <pjagtap@avaya.com>
2020-04-18 14:00:38 +0530

    Auto-generated commit

--- src/environments/environment.stg.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  url:'https://readynow-stg.avaya.com/readynow/api/services',
-  SSO_URL: 'https://readynow-stg.avaya.com/readynow',
-  logOutUrl: 'https://login-stg.avaya.com/sso/common/logout.jsp',
-  adminurl:'https://readynow-stg.avaya.com/readynow/admin',
-  callLocalJson: false,
-  assetsUrl: 'readynow/static/assets/'
-};
